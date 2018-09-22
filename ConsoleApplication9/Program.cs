272876e2b92d1ba920ae5d0e99129bf369e907ec
joelastley555 <joel.astley@gmail.com>
2018-09-22 12:30:41 +0100

    Added start-up text

--- ConsoleApplication9/Program.cs
+++ ConsoleApplication9/Program.cs
@@ -18,6 +18,21 @@ namespace ConsoleApplication9
 
              Version 0.0-initial
             */
+
+            Console.Title = "J E R K // Text Adventures by joelastley555";
+            Console.WriteLine("J E R K");
+            Console.WriteLine();
+            Console.WriteLine("A text adventures game created by joelastley555.");
+            Console.WriteLine("Inspired by the old classic game, Zork");
+            Console.WriteLine();
+            Console.WriteLine("GitHub: https://github.com/joelastley555/J-E-R-K");
+            Console.WriteLine("Protected by MIT License. To find it, goto the GitHub above");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to play...");
+            Console.ReadKey();
         }
     }
 }
