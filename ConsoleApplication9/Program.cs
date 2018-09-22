4b8ef1b88436672430f5e21dd089d210d1254bfa
Joel (Modern Gaming) <Joel (Modern Gaming)@DESKTOP-O0VLG0Q>
2018-09-22 11:56:37 +0100

    Add project files.

---
+++ ConsoleApplication9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             ╔╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟╤╗
+●════════════╣J E R K text adventures╠════════════●
+●════════════╣   By  joelastley555  ͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟ ╠════════════●
+             ╚╧╧╧╧╧╧╧╧╧╧╧╧╧╧╧╧╧╧╧╧╧╧╧╝
+
+             Version 0.0-initial
+            */
+        }
+    }
+}
